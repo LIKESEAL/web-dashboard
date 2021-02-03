d1bef2c296c7581958e2d4c73b6477f1cb40173f
Seulwoo <gmgkglgh@naver.com>
2021-02-03 23:44:44 +0900

    [ Feat ] 슬우 페이지에 샘플 라디오 버튼 생성 : SW (#5)

--- src/pages/Ji/index.jsx
+++ src/pages/Ji/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 function Ji() {
   return (
     <div>
--- src/pages/Seul/index.jsx
+++ src/pages/Seul/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TempRadio from '../../components/Radio/TempRadio';
 
 function Seul() {
   return (
     <div>
       슬우
+      <TempRadio text={"대충 만든 라디오버튼"}/>
     </div>
   );
 }

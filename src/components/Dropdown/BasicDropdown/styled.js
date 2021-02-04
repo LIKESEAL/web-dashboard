5d4b63114c21cbba3ae094e243c5c9c9f84b1868
Jiyooung <jiyoung.simm@gmail.com>
2021-02-05 03:06:46 +0900

    [ CREATE ] 지영 페이지에 샘플 드롭다운 버튼 생성 : JY (#4)

---
+++ src/components/Dropdown/BasicDropdown/styled.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+`;
+
+const StyledLi = styled.li`
+  float: left;
+`;
+
+const Dropbtn = styled.div`
+  display: inline-block;
+  color: white;
+  text-align: center;
+  min-width: 70px;
+  max-width: 150px;
+  padding: 14px 16px;
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+const DropDownContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 100px;
+  max-width: 150px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const DropDownLi = styled(StyledLi)`
+  display: inline-block;
+  &:hover {
+    background-color: red;
+  }
+  &:hover ${DropDownContent} {
+    display: block;
+  }
+`;
+
+const StyledA = styled.a`
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  &:hover {
+    background-color: red;
+  }
+`;
+
+const SubA = styled.a`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
+
+export default {
+  StyledUl,
+  StyledLi,
+  Dropbtn,
+  DropDownContent,
+  DropDownLi,
+  StyledA,
+  SubA
+}
--- src/pages/Ji/index.jsx
+++ src/pages/Ji/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import BasicDropdown from '../../components/Dropdown/BasicDropdown';
 
 function Ji() {
   return (
     <div>
-        지영
+      지영
+      <div>
+        <BasicDropdown contents={["심지영", "박슬우", "이세연"]} />
+      </div>
     </div>
   );
 }

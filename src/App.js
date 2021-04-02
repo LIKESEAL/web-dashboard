265e418034e12d0768f439862819f22a4008d2de
Seyoun Lee <54106189+youn16@users.noreply.github.com>
2021-04-02 10:51:49 +0900

    [ADD] react-hot-loader 추가
    코드 변경시 새로 로딩하지 않아도 자동 적용 되게 해주는 라이브러리(react-hot-loader) 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { hot } from 'react-hot-loader'
 import { BrowserRouter as Router } from "react-router-dom";
 import { PagesContent } from './pages';
+
+const basePath = process.env.BASE_PATH || '/';
+
 function App() {
   return (
-    <Router>
+    <Router basename={ basePath }>
       <PagesContent />
     </Router>
   );
 }
 
-export default App;
+export default hot(module)(App);
